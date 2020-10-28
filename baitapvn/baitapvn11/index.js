49f9d4c6ee92bec66481eb274b4ab67f581e739e
nguyentuan <tuanech20@gmail.com>
2020-10-28 23:07:32 +0700

    baivn11

---
+++ baitapvn/baitapvn11/index.js
@@ -0,0 +1,19 @@
+function doF(doc){
+    return (`độ F đổi từ ${doc} độ C là : ${(doc*1.8)+32} độ F`);
+}
+
+function doC(dof){
+    return (`độ C đổi từ ${dof} độ F là : ${(dof-32)/1.8} độ C`);
+}
+
+function tinhtheKY(nam){
+    return Math.ceil(nam/100);
+}
+
+function catChuoi(str){
+    return (`chuỗi cắt được là : ${str.substring(0,10)}...`)
+}
+
+function chuHoa(text){
+    return (`${(text.substring(0,1)).toUpperCase()}${(text.substring(1)).toLowerCase()}`)
+}
