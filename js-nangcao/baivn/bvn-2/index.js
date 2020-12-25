6a917b07ef2c1c5e08880552632b84d02ebab927
tuansaodo <71915567+tuansaodo@users.noreply.github.com>
2020-12-25 11:32:24 +0700

    baivn2

--- js-nangcao/baivn/bvn-2/index.js
+++ js-nangcao/baivn/bvn-2/index.js
@@ -1,5 +1,5 @@
 //bai1//
-function findTheString(arr) {
+function Bai1(arr) {
     let max = arr[0].length;
     let maxstring=arr[0];
     for (i = 1; i < arr.length; i++) {
@@ -11,7 +11,7 @@ function findTheString(arr) {
     return maxstring;
   }
 // bai 2
-function arrcomparison(arr1,arr2){
+function  Bai2(arr1,arr2){
     arr = []
     for(let i = 0 ; i < arr1.length ; i++){
         if ( arr2.indexOf(arr1[i],0) == -1){
@@ -27,7 +27,7 @@ function arrcomparison(arr1,arr2){
     return arr
 }
 //bai4 
-function ageTb(arr){
+function bai4(arr){
     let Tb = []
     let tong = 0
 
@@ -42,6 +42,16 @@ function ageTb(arr){
     return agetb
 }
 // bai 5
-function sortAge(arr){
+function Bai5(arr){
     return arr.sort(function(a,b){return b.age - a.age})
+}
+//bai 6
+function Bai6(arr){
+    return arr.sort(function(a, b){
+        var x = a.name.toLowerCase();
+        var y = b.name.toLowerCase();
+        if (x < y) {return -1;}
+        if (x > y) {return 1;}
+        return 0;
+      });
 }
